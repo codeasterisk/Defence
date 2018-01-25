8bfb115fe65ef5a19ac408a2fae6c91a9e1f7ada
Abdulla Al-Sayyed <abdulla.a.alsayyed@gmail.com>
2018-01-26 00:13:35 +0200

    adding website controllers

--- app/Http/Controllers/Website/FilltersController.php
+++ app/Http/Controllers/Website/FilltersController.php
@@ -5,29 +5,9 @@ namespace App\Http\Controllers\Website;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class HomeController
+class FilltersController
 {
-    public function index() {
-        return view('website.welcome');
-    }
-
-    public function getBreackingNews(Request $request):JsonResponse {
-
-    }
-
-    public function getLatest(Request $request):JsonResponse {
-
-    }
-
-    public function getNewsByCategory(Request $request):JsonResponse {
-
-    }
-
-    public function getMostRead(Request $request):JsonResponse {
-
-    }
-
-    public function getInfographs(Request $request):JsonResponse {
-
+    public function ByCategory() {
+        return view('website.category');
     }
 }
--- app/Http/Controllers/Website/HomeController.php
+++ app/Http/Controllers/Website/HomeController.php
@@ -1,13 +1,46 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Website;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Category;
+use App\Gallery;
+use App\News;
+use App\SearchLog;
+use App\Video;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
-class Controller extends BaseController
+class HomeController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index() {
+        return view('website.welcome')
+            ->with('categories', Category::all())
+            ->with('latest_news', News::orderby('created_at', 'desc')->get()->take(5))
+            ->with('pane_news', Category::where(['type' => 'news'])->get())
+            ->with('pane_latest', Category::where(['type' => 'news'])->orderBy('created_at', 'desc')->get())
+            ->with('trendings', News::orderBy('clicks', 'desc')->get())
+            ->with('videos', Video::orderBy('created_at', 'desc')->get())
+            ->with('infographs', Gallery::all()->take(3))
+            ->with('trending_searches', SearchLog::orderBy('clicks', 'desc')->get()->take(10));
+    }
+
+    public function getBreackingNews(Request $request):JsonResponse {
+
+    }
+
+    public function getLatest(Request $request):JsonResponse {
+
+    }
+
+    public function getNewsByCategory(Request $request):JsonResponse {
+
+    }
+
+    public function getMostRead(Request $request):JsonResponse {
+
+    }
+
+    public function getInfographs(Request $request):JsonResponse {
+
+    }
 }
